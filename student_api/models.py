c7b60a21b9eaa876b5896a9a151b4d3917f787fd
Himmel König <itogreniyorum@gmail.com>
2021-11-06 13:25:53 +0300

    Migrated

--- student_api/models.py
+++ student_api/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+class Path(models.Model):
+  path_name = models.CharField(max_length = 30)
+  def __str__(self):
+    	return self.path_name
+  
 class Student(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    number = models.IntegerField(blank=True, null=True)
-
-    def __str__(self):
-        return f"{self.last_name} {self.first_name}"
+  path = models.ForeignKey(Path, related_name= "students", on_delete=models.CASCADE)
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+  number = models.IntegerField(blank=True, null=True)
+  def __str__(self):
+    return f"{self.last_name} {self.first_name}"
--- student_api/serializers.py
+++ student_api/serializers.py
@@ -1,14 +1,21 @@
 from rest_framework import serializers
-from .models import Student
+from .models import Student, Path
 
 # class StudentSerializerWithSerializer(serializers.Serializer):
 #     first_name = serializers.CharField(max_length=30)
 #     last_name = serializers.CharField(max_length=30)
 #     number = serializers.IntegerField(required=False)
 
+class PathSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Path
+    fields = '__all__'
+  
 class StudentSerializer(serializers.ModelSerializer):
+  path = serializers.StringRelatedField()
+  path_id = serializers.IntegerField()
   class Meta:
     model = Student
-    fields = ['id', 'first_name', 'last_name', 'number']
+    fields = ['id', 'first_name', 'last_name', 'number', 'path', 'path_id']
     # fields = '__all__'
     # excude = ['number']
